1579918b1867bb37959cc47387192e6f619ab1d3
Joochh <125891712+Joochh@users.noreply.github.com>
2024-08-12 00:15:03 +0800

    Update connection.php

--- controller/connection.php
+++ controller/connection.php
@@ -1,12 +1,12 @@
 <?php
-
-$databaseHost = "localhost";
-$databaseName = "mghs";
-$databaseUsername = 'root';
-$databasePassword = '';
+$databaseHost = getenv('DB_HOST');
+$databaseName = getenv('DB_NAME');
+$databaseUsername = getenv('DB_USER');
+$databasePassword = getenv('DB_PASS');
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
-
-
+if (!$mysqli) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
